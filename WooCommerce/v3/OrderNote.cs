10a4e08b43f9b088375e252771c3c6cbd4ef51ce
James Yang <xiaofaye@msn.com>
2020-04-29 09:48:25 +1200

    Version 0.8.2 - Part 2
    4. Missing property in OrderNote. #451

--- WooCommerce/v3/OrderNote.cs
+++ WooCommerce/v3/OrderNote.cs
@@ -4,5 +4,18 @@ using System.Runtime.Serialization;
 namespace WooCommerceNET.WooCommerce.v3
 {
     [DataContract]
-    public class OrderNote : v2.OrderNote { }
+    public class OrderNote : v2.OrderNote 
+    {
+        /// <summary>
+        /// Order note author.
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string author { get; set; }
+
+        /// <summary>
+        /// If true, this note will be attributed to the current user. If false, the note will be attributed to the system. Default is false.
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public bool added_by_user { get; set; }
+    }
 }
