20d64253b34eae7b79eacba69b78e49f96832df6
Patryk Krzoska <patryk.krzoska@autopartner.com>
2024-11-04 08:48:29 +0100

    https://github.com/XiaoFaye/WooCommerce.NET/issues/785

--- RestAPI.cs
+++ RestAPI.cs
@@ -223,7 +223,9 @@ namespace WooCommerceNET
                 // start the stream immediately
                 httpWebRequest.Method = method.ToString();
                 httpWebRequest.AllowReadStreamBuffering = false;
-                
+                //https://github.com/XiaoFaye/WooCommerce.NET/issues/785
+                httpWebRequest.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
+
                 if (webRequestFilter != null)
                     webRequestFilter.Invoke(httpWebRequest);
 
